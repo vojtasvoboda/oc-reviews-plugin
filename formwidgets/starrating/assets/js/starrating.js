bd54bc8b7e6eb19c2a6f777eef87c37fdb85a17e
Vojta Svoboda <vojtasvoboda.cz@gmail.com>
2016-12-12 12:35:03 +0100

    Fix code style

--- formwidgets/starrating/assets/js/starrating.js
+++ formwidgets/starrating/assets/js/starrating.js
@@ -1,19 +1,19 @@
 $(document).ready(function() {
 
-    $('.rateyo').each(function() {
+    $(".rateyo").each(function() {
 
         $(this).rateYo({
-            rating: typeof $(this).data('input-id') !== "undefined" ? parseInt($('#' + $(this).data('input-id')).val()) : 0,
+            rating: typeof $(this).data("input-id") !== "undefined" ? parseInt($("#" + $(this).data("input-id")).val()) : 0,
             numStars: 5,
             minValue: 1,
             maxValue: 5,
             fullStar: true,
-            readOnly: typeof $(this).data('readonly') !== "undefined" ? $(this).data('readonly') : false
+            readOnly: typeof $(this).data("readonly") !== "undefined" ? $(this).data("readonly") : false
 
         }).on("rateyo.set", function (e, data) {
-            var input = $(this).data('input-id');
+            var input = $(this).data("input-id");
             if (typeof input !== "undefined") {
-                $('#' + input).val(data.rating);
+                $("#" + input).val(data.rating);
             }
         });
     });
