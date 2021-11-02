a4e25a946ffef3c26ded9eafbdd13830b2631e0c
dmotriy.smotrikov@gmail.com <abcZXC1982049>
2021-11-02 15:38:25 +0300

    Change `vojtasvoboda_reviews_reviews`.`sort_order` column type from boolean to integer.

---
+++ updates/change_sort_order_column_type.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Smoptrikov\Reviews\Updates;
+
+use October\Rain\Database\Schema\Blueprint;
+use October\Rain\Database\Updates\Migration;
+use Schema;
+
+
+class ChangeSortOrderColumnType extends Migration
+{
+    public function up()
+    {
+        Schema::table('vojtasvoboda_reviews_reviews', static function (Blueprint $table) {
+            $table->integer('sort_order')->default(0)->change();
+        });
+    }
+
+    public function down()
+    {
+        Schema::table('vdlp_redirect_clients', static function (Blueprint $table) {
+            $table->boolean('sort_order')->nullable()->change();
+        });
+    }
+}
