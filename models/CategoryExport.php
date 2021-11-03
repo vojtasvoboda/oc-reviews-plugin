0b148a00f21add78480208788f57546864b91348
dmotriy.smotrikov@gmail.com <abcZXC1982049>
2021-11-03 17:52:17 +0300

    fix: missing table attribute

--- models/CategoryExport.php
+++ models/CategoryExport.php
@@ -5,6 +5,8 @@ use Backend\Models\ExportModel;
 
 class CategoryExport extends ExportModel
 {
+    public $table = 'vojtasvoboda_reviews_categories';
+
     public function exportData($columns, $sessionKey = null)
     {
         return self::make()
